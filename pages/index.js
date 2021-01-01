41f6bea608b7467361b84c440a4d6bd80b922ca6
Alex Lushiku <alex.lushiku@outlook.com>
2021-01-01 22:33:05 +0100

    remove next/image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from './components/Header'
 import Home from './components/home/Home'
 import About from './components/home/About'
