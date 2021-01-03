572055a568ce2f84fc7a8f428ae53cf0bb7ce878
Noodles <anxlus@tuta.io>
2021-01-03 04:20:36 +0100

    reposition title

--- pages/components/home/Home.js
+++ pages/components/home/Home.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Home = () => {
   return(
     <div className='flex lg:flex-row justify-center items-center h-full max-md:w-3/5'>
-      <div className='lg:relative maxmd:w-4/5 lg:bottom-20 md:mr-4'>
+      <div className='relative maxmd:w-4/5 bottom-20 md:mr-4'>
         <h1 className='text-4xl font-bold mb-4 leading-snug'>Accurate English and Native <br className='maxmd:hidden'></br>Dutch Translation</h1>
         <p className='mb-6'>
           Do you have documents, files, or any other text which need<br className='maxmd:hidden'></br>to be translated from English to Dutch or from<br className='maxmd:hidden'></br>Dutch to English?</p>
