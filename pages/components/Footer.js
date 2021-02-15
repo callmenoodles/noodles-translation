39658e66d0ed5fadaf2b88579aeb76f6835f1cdc
Alex Lushiku <anxlus@tuta.io>
2021-02-15 17:47:01 +0100

    add CoC to footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,8 +2,9 @@ import Link from 'next/link'
 
 const Footer = () => {
   return(
-    <div className='border-t maxmd:h-24 md:h-16 w-11/12 flex maxmd:flex-col md:flex-row justify-evenly items-center'>
-      <h3>Made by <Link href='https://noodles.services'><a className='text-primary'><u>Noodles</u></a></Link> with Next.js</h3>
+    <div className='border-t maxmd:h-32 md:h-16 w-11/12 flex maxmd:flex-col md:flex-row justify-evenly items-center'>      
+      <h3>Developed by <Link href='https://noodles.services'><a className='text-primary'><u>Noodles</u></a></Link> with Next.js</h3>
+      <h3>CoC: <span className='text-primary'>81879342</span></h3>
       <h3>Illustrations by <Link href='https://twitter.com/ninaLimpi'><a className='text-primary'><u>Katerina Limpitsouni</u></a></Link></h3>
     </div>
   )
